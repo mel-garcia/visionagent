16b9c24206d4318751fd14f089eec7f4f70e1989
Andhy <andhygonzalez25@gmail.com>
2024-12-11 15:13:51 -0500

    Footer+Textbox

---
+++ visionagentpage/src/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './assets/Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1></h1>
+            <nav>
+                <ul>
+                    <li style={{ marginRight: '15px' }}>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li style={{ marginRight: '15px' }}>
+                        <a href="#about" >About</a>
+                    </li>
+                    
+
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
