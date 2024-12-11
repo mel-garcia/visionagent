7edd74c40bb5ea96e44255628be5174b1590f077
Andhy <andhygonzalez25@gmail.com>
2024-12-11 14:22:17 -0500

    Redo
    Had to add and remove certain elements due to error

--- visionagentpage/src/App.jsx
+++ visionagentpage/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import Header from "./Header";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
