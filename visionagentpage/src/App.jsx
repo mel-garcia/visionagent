d52afee2aac089796f6edc13ef97d7caec7895f4
Andhy <andhygonzalez25@gmail.com>
2024-12-11 16:02:34 -0500

    Downloads and Navbar stuff
    Footer and navbar experiments with downloads for them

--- visionagentpage/src/App.jsx
+++ visionagentpage/src/App.jsx
@@ -8,6 +8,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [message, setMessage] = useState(''); // State to control the message below the input
+
+  const handleClick = () => {
+    setMessage('submit'); // Set the message to "submit" when the button is clicked
+  };
+
   return (
     <>
                 <Header />
@@ -24,7 +30,19 @@ function App() {
       <h1>Enter URL Here!</h1> <br></br>
       <p>It'll Summarize Your Website</p>
 
-      <input name="myInput" />
+
+      <input name="myInput" placeholder="Type something..." />
+      <button
+        onClick={handleClick}
+        style={{ marginLeft: '10px', padding: '5px 10px', cursor: 'pointer' }}
+      >
+        Click Me
+      </button>
+      <div style={{ marginTop: '10px', color: 'blue' }}>
+        {message} {/* Display the message below the input */}
+      </div>
+
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
